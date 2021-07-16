69c77053597c61611589122e681a0e3c00299b98
keenson <keensono@gmail.com>
2021-07-16 04:47:03 +0100

    static files and theme instalation

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -120,11 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+    # BASE_DIR / 'static'
+]
 
-
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
