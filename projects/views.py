90181966efb3e475e545fd0e1c6dfcfcc0dd2e1b
keenson <keensono@gmail.com>
2021-07-28 12:03:02 +0100

    feat: add user actions

--- projects/views.py
+++ projects/views.py
@@ -6,63 +6,70 @@ from django.http import HttpResponse
 from .forms import ProjectForm
 
 
-
 def projects(request):
     projects = Project.objects.all()
-    context = { 'projects': projects}
-    
-    return render(request, 'projects/projects.html', context)
+    context = {"projects": projects}
+
+    return render(request, "projects/projects.html", context)
 
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    print('projectObj:', projectObj)
-    
-    
-    return render(request, 'projects/single-project.html', {'project': projectObj,  })
+    print("projectObj:", projectObj)
+
+    return render(
+        request,
+        "projects/single-project.html",
+        {
+            "project": projectObj,
+        },
+    )
 
 
-@login_required(login_url='login')
-def createProject(request,):
+@login_required(login_url="login")
+def createProject(
+    request,
+):
+    profile = request.user.profile
     form = ProjectForm()
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('projects')
-    
-    context = {'form': form}
-    
-    return render(request, 'projects/project_form.html', context)
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            return redirect("account")
+
+    context = {"form": form}
+
+    return render(request, "projects/project_form.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = ProjectForm(request.POST, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
-    
-    context = {'form': form}
-    
-    return render(request, 'projects/project_form.html', context)
+            return redirect("account")
+
+    context = {"form": form}
+
+    return render(request, "projects/project_form.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
-    if request.method == 'POST':
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    if request.method == "POST":
         project.delete()
-        return redirect('projects')
-    
-    context = {'object': project}
-    return render(request, 'projects/delete_template.html', context)
-    
+        return redirect("projects")
 
-
-
+    context = {"object": project}
+    return render(request, "delete_template.html", context)
--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,3 @@
-
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
@@ -7,10 +6,9 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 
-
 # @receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
-    print('Profile signal triggered')
+    print("Profile signal triggered")
     if created:
         user = instance
         profile = Profile.objects.create(
@@ -19,12 +17,24 @@ def createProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
         )
-    
-    
+
+
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
     user.delete()
-    
+
+
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateUser, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)
-    
