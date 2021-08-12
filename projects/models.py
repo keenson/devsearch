5dda27c9434ad505b41eb5ede6a46ffb9aa83092
keenson <keensono@gmail.com>
2021-08-12 04:36:34 +0100

    add pagination

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from users.models import Profile
+
 # Create your models here.
 
 
@@ -9,53 +10,57 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
-    demo_link = models.CharField(max_length=2000,null=True, blank=True)
-    source_link = models.CharField(max_length=2000,null=True, blank=True)
-    tags = models.ManyToManyField('Tag', blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False ) 
-    
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
     def __str__(self):
         return self.title
-    
+
     @property
     def imageURL(self):
         try:
             img = self.featured_image.url
         except:
-            img = ''
-            
+            img = ""
+
         return img
-    
-    
-    
-    
-    
+
+    class Meta:
+        ordering = ["created"]
+
+
 class Review(models.Model):
     VOTE_TYPE = (
-        ('up', 'Up Vote'),
-        ('down', 'Down Vote'),
+        ("up", "Up Vote"),
+        ("down", "Down Vote"),
     )
-    
-    #owner
-    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+
+    # owner
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False ) 
-    
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
     def __str__(self):
         return self.value
-    
-    
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False )
-    
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
     def __str__(self):
         return self.name
-    
-    
--- projects/views.py
+++ projects/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-
 from .models import Project, Tag
 
 from django.http import HttpResponse
 from .forms import ProjectForm
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
 
 
 def projects(request):
 
     projects, search_query = searchProjects(request)
+    custom_range, projects = paginateProjects(request, projects, 2)
 
-    context = {"projects": projects, "search_query": search_query}
-
+    context = {
+        "projects": projects,
+        "search_query": search_query,
+        "custom_range": custom_range,
+    }
     return render(request, "projects/projects.html", context)
 
 
